c4cdd22fe0447b7ef6f6718042cf8d15169802a9
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2024-05-14 22:48:11 -0300

    chore: create query group

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,7 +2,7 @@ export function buildRoutePath(path) {
     const routeParametersRegex = /:([a-zA-Z]+)/g;
     const pathWithParameters = path.replace(routeParametersRegex, '(?<$1>[a-zA-Z0-9\_-]+)');
 
-    const pathRegex = new RegExp(`^${pathWithParameters}$`);
+    const pathRegex = new RegExp(`^${pathWithParameters}(?<query>\\?(.*))?$`);
 
     return pathRegex;
 }
